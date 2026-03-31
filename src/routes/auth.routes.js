614d4d0df92f840e4399d9689d2a9f920e423b8c
Govindvarshney001 <Govind.work001@example.com>
2026-03-31 10:43:51 +0530

    register and login controller added

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const authController = require("../controllers/auth.controller");
+const userModel = require("../models/user.model");
+const userLoginController = require("../controllers/auth.controller");
+const cookieParser = require("cookie-parser");
+
+const router = express.Router();
+
+/* POST /api/auth/register */
+router.post("/register", authController.userRegisterController);
+
+/* POST /api/auth/login */
+router.post("/login", authController.userLoginController);
+
+// /**
+//  * - POST /api/auth/logout
+//  */
+// router.post("/logout", authController.userLogoutController);
+
+module.exports = router;
